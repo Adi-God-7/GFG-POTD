640bfa2e1b0454ade8a8dc4b81d6c90adea3965a
Aditya Kumar <manorama.rajkumar@gmail.com>
2026-08-13 21:43:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Longest Path in a Directed Acyclic Graph/longest-path-in-a-directed-acyclic-graph.py
@@ -0,0 +1,45 @@
+class Solution:
+    def maxDistance(self, V, src, edges):
+        adj = [[] for _ in range(V)]
+        indegree = [0] * V
+
+        for u, v, w in edges:
+            adj[u].append((v, w))
+            indegree[v] += 1
+
+        from collections import deque
+
+        q = deque()
+
+        for i in range(V):
+            if indegree[i] == 0:
+                q.append(i)
+
+        topo = []
+
+        while q:
+            u = q.popleft()
+            topo.append(u)
+
+            for v, w in adj[u]:
+                indegree[v] -= 1
+                if indegree[v] == 0:
+                    q.append(v)
+
+        dist = [-10**18] * V
+        dist[src] = 0
+
+        for u in topo:
+            if dist[u] == -10**18:
+                continue
+
+            for v, w in adj[u]:
+                dist[v] = max(dist[v], dist[u] + w)
+
+        INT_MIN = -2**31
+
+        for i in range(V):
+            if dist[i] == -10**18:
+                dist[i] = INT_MIN
+
+        return dist
